f350a93ed6fc454007d5ef26a689b84a9a6ec8e2
Erhezi <45082266+Erhezi@users.noreply.github.com>
2025-05-22 11:23:34 -0400

    more things to put on server

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ from waitress import serve
 app = create_app()
 
 if __name__ == '__main__':
-    # print("Starting Flask application with restructured architecture...")
-    # app.run(debug=True)
+    print("Starting Flask application with restructured architecture...")
+    app.run(debug=True)
 
-    print("Starting Waitress server...")
-    serve(app, host='0.0.0.0', port=8090)
+    # print("Starting Waitress server...")
+    # serve(app, host='0.0.0.0', port=8090)
