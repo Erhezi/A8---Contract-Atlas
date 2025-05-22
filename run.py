1dde015f68d6f4a8c94d97182b80f74e33c11302
Erhezi <45082266+Erhezi@users.noreply.github.com>
2025-05-22 09:34:01 -0400

    updated requirement and run to add waitress

--- run.py
+++ run.py
@@ -2,10 +2,14 @@
 # This serves as the entry point for running the Flask application
 
 from preprocessorEC import create_app
+from waitress import serve
 
 # Create the application using our factory function
 app = create_app()
 
 if __name__ == '__main__':
-    print("Starting Flask application with restructured architecture...")
-    app.run(debug=True)
+    # print("Starting Flask application with restructured architecture...")
+    # app.run(debug=True)
+
+    print("Starting Waitress server...")
+    serve(app, host='0.0.0.0', port=8090)
