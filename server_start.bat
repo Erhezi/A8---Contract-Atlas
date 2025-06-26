49abe5c2865c5b1f850758d8f68f3b059d691099
Erhezi <45082266+Erhezi@users.noreply.github.com>
2025-06-26 10:52:28 -0400

    changed server_start.bat

--- server_start.bat
+++ server_start.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Activate the virtual environment first
-call "C:\path\to\your\server\venv\Scripts\activate.bat"
+call "D:\PreProcessor\A8---Contract-Atlas\venv\Scripts\activate.bat"
 
 set FLASK_ENV=production
 set URL_PREFIX=/preprocessor
